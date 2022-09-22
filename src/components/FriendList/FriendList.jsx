c175c01cd0fd7b0d75475cfaaf8f7bab5f2d22f0
Bohdana Lozenko <102677592+beevine@users.noreply.github.com>
2022-09-22 03:32:07 +0300

    solves tasks and adds styles

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css['friend-list']}>
+      {friends.map(friend => (
+        <FriendListItem
+          isOnline={friend.isOnline}
+          avatar={friend.avatar}
+          name={friend.name}
+          key={friend.id}
+        ></FriendListItem>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number.isRequired })
+  ).isRequired,
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+
+      <ul className={css['stat-list']}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            key={id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
